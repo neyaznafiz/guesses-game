1ba095c3b4630ffb24b8f23bdcf4886d4d49ead4
NEYAZ N <neyaznafiz@outlok.com>
2023-06-16 19:04:28 +0600

    loop and checkint erro implimented

--- src/guess_game.rs
+++ src/guess_game.rs
@@ -3,26 +3,36 @@ use std::io;
 use std::cmp::Ordering;
 
 pub fn guess() {
-    println!("Guess the number!");
+  println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+  let secret_number = rand::thread_rng().gen_range(1..=100);
     // println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+  println!("Please input your guess");
 
+  loop {
     let mut guesses = String::new();
     io::stdin()
         .read_line(&mut guesses)
         .expect("Failed to read line");
 
-    let guesses: u32 = guesses.trim().parse()
-                       .expect("Please type a number");
+    let guesses: u32 = match guesses.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("Pleae input a number as guess!");
+				continue;
+			},
+		};
 
     // println!("You guessed: {guesses}");
 
     match guesses.cmp(&secret_number) {
         Ordering::Less => println!("Your number {guesses} is too small than secret number {secret_number}"),
         Ordering::Greater => println!("Your number {guesses} is too big than secret number {secret_number}"),
-        Ordering::Equal => println!("YEY! You gussed the right number."),
+        Ordering::Equal => {
+					println!("YEY! You gussed the right number.");
+					break;
+				}
     }
+  }
 }
