3924d78ec5404b3702c297b480883c2ab3e6a55c
NEYAZ N <neyaznafiz@outlok.com>
2023-06-28 15:03:39 +0600

    v2 code rewriting

--- guess-game-v2/src/match_number/check_value.rs
+++ guess-game-v2/src/match_number/check_value.rs
@@ -1,11 +1,9 @@
 use super::user_input;
 
-pub fn check_value() {
+pub fn check_value() -> Result<u32, String> {
   let input_value = user_input::user_input();
-  let check_input: u32 = match input_value.parse() {
-    Ok(num) => num,
-    Err(_)=> {
-     return println!("Please input a number as guess");
-    }
-  };
+  match input_value.parse() {
+    Ok(num) => Ok(num),
+    Err(_) => {Err("Please input a number as guess".to_string())}
+  }
 }
